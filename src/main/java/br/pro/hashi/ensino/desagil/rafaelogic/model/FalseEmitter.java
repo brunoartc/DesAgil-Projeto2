c07339976b3f713be75f6be0c5dad53d9994840d
Bruno <brunoartc@yahoo.com>
2018-03-28 00:08:58 -0300

    fix all errors

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/FalseEmitter.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/FalseEmitter.java
@@ -2,9 +2,8 @@ package br.pro.hashi.ensino.desagil.rafaelogic.model;
 
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Emitter;
 
-public class TrueEmitter implements Emitter {
-	@Override
+public class FalseEmitter implements Emitter {
 	public boolean read() {
-		return true;
+		return false;
 	}
 }
--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/TrueEmitter.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/TrueEmitter.java
@@ -2,9 +2,8 @@ package br.pro.hashi.ensino.desagil.rafaelogic.model;
 
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Emitter;
 
-public class FalseEmitter implements Emitter {
-	@Override
+public class TrueEmitter implements Emitter {
 	public boolean read() {
-		return false;
+		return true;
 	}
 }
