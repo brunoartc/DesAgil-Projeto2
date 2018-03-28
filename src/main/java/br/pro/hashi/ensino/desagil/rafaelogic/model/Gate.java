9b863270ae653ff0390d461f5256adae313b5261
Bruno <brunoartc@yahoo.com>
2018-03-27 23:53:22 -0300

    initial commit form hashiprobr/desagil-projeto2  with improvements

---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/Gate.java
@@ -0,0 +1,15 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
+
+public abstract class Gate implements Emitter, Receiver {
+
+	public boolean read() {
+		// TODO Auto-generated method stub
+		
+		return false;
+	}
+
+	public void connect(int pinIndex, Emitter emitter) {
+		// TODO Auto-generated method stub
+		
+	}
+}
