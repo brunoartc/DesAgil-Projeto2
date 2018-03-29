b6ad03c3ddf9ad7e545da0ed9c4a028cc85d4752
Bruno <brunoartc@yahoo.com>
2018-03-29 20:09:25 -0300

    full files

---
+++ src/test/java/br/pro/hashi/ensino/desagil/rafaelogic/model/Mytest.java
@@ -0,0 +1,14 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Mytest {
+
+	@Test
+	public void test() {
+		System.out.print(new FalseEmitter().read());
+	}
+
+}
