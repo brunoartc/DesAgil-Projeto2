75cc70ed9c5274574482e164bff6f26f02952d13
Bruno <brunoartc@yahoo.com>
2018-03-29 20:39:38 -0300

    add not

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/NotGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/NotGate.java
@@ -1,5 +1,27 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.model;
 
 public class NotGate extends Gate{
+	private NandGate gate;
+	private Emitter[] emitters;
+	private boolean Nand1;
+
+	public NotGate() {
+		emitters = new Emitter[1];
+		gate = new NandGate();
+	}
+
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		emitters[pinIndex] = emitter;
+	}
+
+	@Override
+	public boolean read() {
+		gate.connect(0, emitters[0]);
+		gate.connect(1, emitters[0]);
+		Nand1 = gate.read();
+		
+		return (Nand1);
+	}
 
 }
