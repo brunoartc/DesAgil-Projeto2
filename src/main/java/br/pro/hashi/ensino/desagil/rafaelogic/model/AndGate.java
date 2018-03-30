37cbf03b1cdb2355e8daf9e24e2187de65e55e7d
vinigl <vinicius.gl098@gmail.com>
2018-03-29 21:20:27 -0300

    And Done

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/AndGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/AndGate.java
@@ -1,5 +1,28 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.model;
 
 public class AndGate extends Gate{
-
+	
+	private NandGate nand = new NandGate();
+	private NotGate  not = new NotGate();
+	private Emitter[] emitters;
+	
+	public AndGate() {
+		emitters = new Emitter[2];
+	}
+	
+	@Override
+	 public void connect(int pinIndex, Emitter emitter) {
+		 emitters[pinIndex] = emitter;
+	}
+	
+	@Override
+	public boolean read() {
+		nand.connect(0, emitters[0]);
+		nand.connect(1, emitters[1]);
+		
+		not.connect(0,nand);
+		
+		return(not.read());
+	}
+	
 }
