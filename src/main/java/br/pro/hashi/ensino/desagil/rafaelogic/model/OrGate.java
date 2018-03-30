706e9cebf8bdd293bed8c6470329abc0cb587d38
wesleygas <wesleygl16@gmail.com>
2018-03-29 21:56:42 -0300

    Implementacao or-gate

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/OrGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/OrGate.java
@@ -1,5 +1,31 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.model;
 
+
+
 public class OrGate extends Gate{
+	private NandGate  nandA = new NandGate();
+	private NandGate  nandB = new NandGate();
+	private NandGate  nandOut = new NandGate();
+
+	public OrGate(){
+
+	}
+
+	public void connect(int pinIndex, Emitter emitter){
+		if(pinIndex == 0){
+			nandA.connect(0, emitter);
+			nandA.connect(1, emitter);
+			nandOut.connect(0,nandA);
+		}
+		else if(pinIndex == 1){
+			nandB.connect(0, emitter);
+			nandB.connect(1, emitter);
+			nandOut.connect(1,nandB);
+		}
+	}
+
+	public boolean read(){	
+		return nandOut.read();
+	}
 
 }
