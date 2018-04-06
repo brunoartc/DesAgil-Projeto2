7cb048fb83ddc1a2e7f6f24eb8c83e3c7d529526
wesleygas <wesleygl16@gmail.com>
2018-04-06 19:14:44 -0300

    Organizacao para jframe

---
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/View.java
@@ -0,0 +1,18 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import br.pro.hashi.ensino.desagil.rafaelogic.model.*;;
+
+
+public class View {
+
+}
